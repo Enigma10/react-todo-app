47ab6ae691d86566527285d7bd5ad6dfc84873a3
Enigma10 <vkrsaini678@gmail.com>
2017-06-23 03:52:41 +0630

    sort todos

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -26,6 +26,18 @@ module.exports = {
     // Filter by searchText
 
     //sort todos with non-completed first
+    filteredTodos.sort((a,b)=>{
+      if(!a.completed && b.completed){
+        return -1;
+      }
+      else if(a.completed && !b.completed){
+        return 1;
+      }
+      else {
+        return 0;
+      }
+    });
+
     return filteredTodos;
   }
 };
