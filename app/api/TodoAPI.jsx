3924ae113d62f7e98ee613148b7e971b79b306f7
Enigma10 <vkrsaini678@gmail.com>
2017-07-04 01:08:05 +0630

    Made robust code with use of redux

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -1,42 +1,45 @@
 var $ = require('jquery');
+
 module.exports = {
-  setTodos: function(todos){
-    if($.isArray(todos)) {
-      localStorage.setItem('todos',JSON.stringify(todos));
+  setTodos: function (todos) {
+    if ($.isArray(todos)) {
+      localStorage.setItem('todos', JSON.stringify(todos));
       return todos;
     }
   },
-  getTodos: function(){
+  getTodos: function () {
     var stringTodos = localStorage.getItem('todos');
     var todos = [];
+
     try {
       todos = JSON.parse(stringTodos);
-    }catch(e) {
+    } catch (e) {
+
     }
+
     return $.isArray(todos) ? todos : [];
   },
-
-  filterTodos: function(todos, showCompleted, searchText) {
+  filterTodos: function (todos, showCompleted, searchText) {
     var filteredTodos = todos;
 
-    //Filter by showCompleted
-    filteredTodos = filteredTodos.filter((todo)=>{
+    // Filter by showCompleted
+    filteredTodos = filteredTodos.filter((todo) => {
       return !todo.completed || showCompleted;
     });
+
     // Filter by searchText
-    filteredTodos = filteredTodos.filter((todo)=>{
-      var todoText = todo.text.toLowerCase();
-    return searchText.length== 0 || todoText.indexOf(searchText)>-1;
-  });
-    //sort todos with non-completed first
-    filteredTodos.sort((a,b)=>{
-      if(!a.completed && b.completed){
+    filteredTodos = filteredTodos.filter((todo) => {
+      var text = todo.text.toLowerCase();
+      return searchText.length === 0 || text.indexOf(searchText) > -1;
+    });
+
+    // Sort todos with non-completed first
+    filteredTodos.sort((a, b) => {
+      if (!a.completed && b.completed) {
         return -1;
-      }
-      else if(a.completed && !b.completed){
+      } else if (a.completed && !b.completed) {
         return 1;
-      }
-      else {
+      } else {
         return 0;
       }
     });
